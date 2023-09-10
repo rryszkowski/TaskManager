37572e571a4818b5cb7c88cafe5d2c1705a2eacc
rryszkowski <rryszkowski.reg@gmail.com>
2023-09-10 09:40:42 +0200

    Enforce password complexity

--- TaskManager.Application/User/Commands/AddUser/AddUserHandler.cs
+++ TaskManager.Application/User/Commands/AddUser/AddUserHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using System.Text.RegularExpressions;
 using TaskManager.Domain.Abstractions;
 
 namespace TaskManager.Application.User.Commands.AddUser;
@@ -19,6 +20,9 @@ public class AddUserHandler : IRequestHandler<AddUserCommand, string>
     {
         var dto = request.Dto;
 
+        if (!IsValidPassword(dto.Password))
+            throw new InvalidOperationException("Password policy not met.");
+
         var user = new Domain.Entities.User(
             dto.Username,
             dto.Password,
@@ -27,5 +31,14 @@ public class AddUserHandler : IRequestHandler<AddUserCommand, string>
             dto.LastName);
 
         return await _userRepository.Create(user);
+    }
+
+    private static bool IsValidPassword(string password)
+    {
+        // At least one upper case letter, one lower case letter, one digit, one special character
+        // and at least 8 characters long
+        const string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+
+        return Regex.IsMatch(password, pattern);
     }
 }
